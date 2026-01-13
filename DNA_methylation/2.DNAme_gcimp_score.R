033c4f9a3c631c1bf372fe3874f360d30b816e0c
Ramya Raviram <ramya617@gmail.com>
2026-01-13 14:35:51 -0500

    Update 2.DNAme_gcimp_score.R

--- DNA_methylation/2.DNAme_gcimp_score.R
+++ DNA_methylation/2.DNAme_gcimp_score.R
@@ -1,11 +1,14 @@
 
-sites = "LTR_ERV"
+
 library(data.table)
 library(dplyr)
 library(GenomicRanges)
 library(parallel)
 library(stringr)
 library(ggplot2)
+
+
+sites = "gcimp684_500"
 files = list.files( pattern = paste0("*binarize.", sites, ".cov$" ))
 files = paste0(files)
 f = as.list(files)
@@ -15,14 +18,9 @@ covs <- mclapply( f , function(x) { m <- fread(x) ;
 m$ID <- paste0( m$V1, m$V2, m$V3, m$V11) ; 
 m <- m[ !duplicated(m$ID) , ]  ; 
 new = m
-if(sites %in% c("gene_body_noTSS1kb", "promoters.1kb", "promoters.enhancers.1kb")){
-  new = m
-  new$V12 = new$V11
-} else if(sites != "promoters"){
-  new = m
-  new$V11 = paste(m$V8, m$V9, m$V10, sep = "_")
-  new$V12 = paste(m$V8, m$V9, m$V10, sep = "_")
-}
+new$V11 = paste(m$V8, m$V9, m$V10, sep = "_")
+new$V12 = paste(m$V8, m$V9, m$V10, sep = "_")
+
 out <- sapply( unique( new$V11 ) , count_cpgs , new  ) ;
 out <- data.frame( t(out) );
 #out <- data.frame( table(new$V12) );
@@ -33,4 +31,42 @@ return(out) }
 fun = function(x,y) merge(x,y, by = c("GENE") , all = T) 
 ## Reduce cell
 results <- Reduce(fun, covs)
-saveRDS(results, paste0("TSS_1kb_binarize_",sites , "_cpgs.rds"))
+saveRDS(results, paste0("binarize_",sites , "_cpgs.rds"))
+
+#Methylation 
+add_reads <- function(x,m) { k = dplyr::filter( m , V11 == x ) ; val = ( sum(k$V5) / length(k$V5) )  ; return( c(x, val) ) } 
+
+covs <- mclapply( f , function(x) { m <- fread(x) ; 
+m$ID <- paste0( m$V1, m$V2, m$V3, m$V11) ; 
+m <- m[ !duplicated(m$ID) , ]  ; 
+new = m
+new$V11 = paste(m$V8, m$V9, m$V10, sep = "_")
+new$V12 = paste(m$V8, m$V9, m$V10, sep = "_")
+out <- sapply( unique( new$V11 ) , add_reads , new  ) ;
+out <- data.frame( t(out) );
+colnames(out) <- c("GENE", basename(x) ) ; 
+return(out) }, mc.cores = 500 
+)
+#save(covs, file = paste0("TSS_1kb_binarize_", sites, "_meth_covs.rds"))
+fun = function(x,y) merge(x,y, by = c("GENE") , all = T) 
+
+## Reduce cell
+results <- Reduce(fun, covs)
+saveRDS(results, paste0("binarize_", sites, "_meth.rds"))
+
+meth = readRDS(paste0("TSS_1kb_binarize_", site, "_meth.rds"))
+meth$GENE <- as.character(meth$GENE)
+meth <- meth[ !is.na(meth$GENE) ,]
+## A gene by cell matrix (inputs are # of meth)
+rownames(meth) <- meth$GENE
+meth <- meth[, -1]
+
+annotation = read.table("annotation.txt", header = T, sep = "\t")
+
+meth_2 = meth[,subset(annotation, cell_type_2 == "Malignant")$meth_cell]
+meth_an = apply(meth_2, 2, function(x) as.numeric(x))
+meth_an = as.data.frame(meth_an)
+rownames(meth_an) = rownames(meth)
+
+annotation$gcimp_score = rowMeans(meth_an, na.rm = T)          
+                
